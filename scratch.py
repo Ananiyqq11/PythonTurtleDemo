4ebac59b6372880c003de1099f492bc044bd3425
Ananiya <ananiya123416@gmail.com>
2022-10-23 09:34:34 +0300

    turtle basics movement

--- scratch.py
+++ scratch.py
@@ -1,3 +1,16 @@
 from turtle import *
+# color('red', 'yellow')
+# begin_fill()
+# while True:
+#    forward(200)
+#    left(170)
+#    if abs(pos()) < 1:
+#        break
+# end_fill()
+forward(150)
+left(250)
+forward(70)
+right(200)
+forward(75)
 circle(75)
 done()
